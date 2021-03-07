5f558aae582000fba8f925ff81e3e17c17fe2ba8
zulhamsy <zulham.sy@gmail.com>
2021-03-07 23:08:44 +0700

    animation, mobile done

--- article-preview/script.js
+++ article-preview/script.js
@@ -1,14 +1,20 @@
 const $ = (x) => document.querySelector(x);
 let state = 'inactive';
 
-$('.btn-share').addEventListener('click', () => {
-	if(state == 'inactive') {
+$('.btn').addEventListener('click', () => {
+	if (state == 'inactive') {
 		state = 'active';
 		// slide share menu to viewport
+		$('#active').classList.add('right-0');
+		$('#active').classList.remove('right-full');
 		// change button fill color to white
+		$('.btn').classList.add('btn-active');
 	} else {
 		state = 'inactive';
 		// slide share menu out of viewport
+		$('#active').classList.remove('right-0');
+		$('#active').classList.add('right-full');
 		// change button fill color to default
+		$('.btn').classList.remove('btn-active');
 	}
 });
