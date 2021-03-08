a8649b3a1561cf5b687b67889af8529af5085ae9
zulhamsy <zulham.sy@gmail.com>
2021-03-08 11:03:58 +0700

    adding opacity animation slide

--- article-preview/script.js
+++ article-preview/script.js
@@ -6,6 +6,7 @@ $('.btn').addEventListener('click', () => {
 		state = 'active';
 		// slide share menu to viewport
 		$('#active').classList.add('right-0');
+		$('#active').classList.add('opacity-100');
 		$('#active').classList.remove('right-full');
 		// change button fill color to white
 		$('.btn').classList.add('btn-active');
@@ -13,6 +14,7 @@ $('.btn').addEventListener('click', () => {
 		state = 'inactive';
 		// slide share menu out of viewport
 		$('#active').classList.remove('right-0');
+		$('#active').classList.remove('opacity-100');
 		$('#active').classList.add('right-full');
 		// change button fill color to default
 		$('.btn').classList.remove('btn-active');
