0985eb479b29f94176a7836a00b369816a9e9cd2
zulhamsy <zulham.sy@gmail.com>
2021-03-26 23:48:21 +0700

    migrate using vite

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    'article-preview/index.html',
+    'article-preview/style.css',
+    'article-preview/script.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
